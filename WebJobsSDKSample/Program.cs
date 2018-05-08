89ccf1ff2eff0107af82757309b17ef8d1d1b11a
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-05-08 17:26:53 +0900

    Add project files.

---
+++ WebJobsSDKSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebJobsSDKSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
